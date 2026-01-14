679111d517a3021674a26e7d452b1daed9882eab
Sitthikorn Khumthong <98000375+cuenobi@users.noreply.github.com>
2026-01-14 15:08:18 +0700

    Add response body fields to ResponseBase component for UI editing

--- src/modules/mappings/components/builder/ResponseBase.tsx
+++ src/modules/mappings/components/builder/ResponseBase.tsx
@@ -3,6 +3,45 @@ import { Trash2, PlusCircle } from 'react-feather'
 import { FieldArray, FormikErrors, FormikTouched, getIn } from 'formik'
 import { Button, Input } from 'edikit'
 import { IMappingFormValues } from '../../types'
+import styled from 'styled-components'
+
+const Textarea = styled.textarea`
+    width: 100%;
+    display: inline-block;
+    padding: 8px 12px;
+    min-height: 120px;
+    font-size: 13px;
+    font-family: 'Monaco', 'Menlo', 'Ubuntu Mono', 'Consolas', 'source-code-pro', monospace;
+    border: none;
+    cursor: pointer;
+    transition: all 200ms ease-in;
+    background: ${props => props.theme.form.input.background};
+    color: ${props => props.theme.form.input.color};
+    border: ${props => props.theme.form.input.border};
+    ${props => props.theme.form.input.css}
+    resize: vertical;
+    
+    &::-webkit-input-placeholder {
+        font-size: 13px;
+        color: ${props => props.theme.form.input.placeholder.color};
+    }
+    
+    &:hover {
+        background: ${props => props.theme.form.input.hover.background};
+        color: ${props => props.theme.form.input.hover.color};
+        border: ${props => props.theme.form.input.hover.border};
+        ${props => props.theme.form.input.hover.css}
+    }
+    
+    &:focus {
+        cursor: auto;
+        outline: transparent;
+        background: ${props => props.theme.form.input.focus.background};
+        color: ${props => props.theme.form.input.focus.color};
+        border: ${props => props.theme.form.input.focus.border};
+        ${props => props.theme.form.input.focus.css}
+    }
+`
 
 interface IResponseBaseProps {
     values: IMappingFormValues
@@ -114,6 +153,60 @@ export default class ResponseBase extends React.Component<IResponseBaseProps> {
                                     )}
                                 </React.Fragment>
                             ))}
+                            <label
+                                htmlFor="responseBody"
+                                style={{
+                                    gridColumnStart: 1,
+                                    gridColumnEnd: 9,
+                                    marginTop: '10px',
+                                }}
+                            >
+                                Response Body (JSON or text)
+                            </label>
+                            <Textarea
+                                id="responseBody"
+                                name="responseBody"
+                                value={values.responseBody || ''}
+                                onChange={onChange}
+                                onBlur={onBlur}
+                                placeholder='{"message": "success", "data": {}}'
+                                style={{
+                                    gridColumnStart: 1,
+                                    gridColumnEnd: 9,
+                                }}
+                            />
+                            {errors.responseBody && touched.responseBody && (
+                                <div style={{ color: 'red', gridColumnStart: 1, gridColumnEnd: 9 }}>
+                                    {errors.responseBody}
+                                </div>
+                            )}
+                            <label
+                                htmlFor="responseBodyFileName"
+                                style={{
+                                    gridColumnStart: 1,
+                                    gridColumnEnd: 9,
+                                    marginTop: '10px',
+                                }}
+                            >
+                                Response Body File Name (optional - use this instead of Response Body)
+                            </label>
+                            <Input
+                                id="responseBodyFileName"
+                                name="responseBodyFileName"
+                                value={values.responseBodyFileName || ''}
+                                onChange={onChange}
+                                onBlur={onBlur}
+                                placeholder="responses/shop-info.json"
+                                style={{
+                                    gridColumnStart: 1,
+                                    gridColumnEnd: 9,
+                                }}
+                            />
+                            {errors.responseBodyFileName && touched.responseBodyFileName && (
+                                <div style={{ color: 'red', gridColumnStart: 1, gridColumnEnd: 9 }}>
+                                    {errors.responseBodyFileName}
+                                </div>
+                            )}
                         </React.Fragment>
                     )
                 }}
